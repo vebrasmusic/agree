39efe55b6dd652fe05c0164179b36617e5e9daec
Andrés Duvvuri <128623334+vebrasmusic@users.noreply.github.com>
2025-09-23 22:07:58 -0700

    clean slate start over

--- test-data/sql_alchemy.py
+++
@@ -1,8 +0,0 @@
-# [agree:user:sqlalchemy]
-class User(Base):
-    __tablename__ = "users"
-    id = Column(Integer, primary_key=True)
-    username = Column(String, unique=True, nullable=False)
-    email = Column(String, unique=True, nullable=False)
-    full_name = Column(String)
-# [agree:end]
